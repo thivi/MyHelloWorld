4abe894f7f0cf64d40a9043289cffc850d31e768
sathiyakugan <sathiyakugan.15@cse.mrt.ac.lk>
2020-12-11 15:24:00 +0530

    Bump version: 0.0.0 → 0.1.0

--- MyHelloWorld/consts.py
+++ MyHelloWorld/consts.py
@@ -1,4 +1,5 @@
 name = 'MyHelloWorld'
+version "0.1.0"
 author = 'Sathiyakugan Balakrishnan <bsathiyakugan@gmail.com>'
 homepage = 'https://github.com/sathiyakugan/MyHelloWorld'
 default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
