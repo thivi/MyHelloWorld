499605cd61598e1ed60366c491b41f8f2fa4eda5
sathiyakugan <sathiyakugan.15@cse.mrt.ac.lk>
2020-12-11 15:13:25 +0530

    add bumb version for the project

---
+++ MyHelloWorld/consts.py
@@ -0,0 +1,10 @@
+name = 'MyHelloWorld'
+author = 'Sathiyakugan Balakrishnan <bsathiyakugan@gmail.com>'
+homepage = 'https://github.com/sathiyakugan/MyHelloWorld'
+default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
+
+default_json_headers = [
+    ('Content-Type', 'application/json'),
+    ('Cache-Control', 'no-store'),
+    ('Pragma', 'no-cache'),
+]
