963075636d29ae0a2d3c0410bf3c2d1b9d12433f
sathiyakugan <sathiyakugan.15@cse.mrt.ac.lk>
2020-12-12 02:51:59 +0530

    Bump version: 0.5.2-dev0 → 0.5.3-dev0

--- MyHelloWorld/consts.py
+++ MyHelloWorld/consts.py
@@ -1,5 +1,5 @@
 name = 'MyHelloWorld'
-version = "version = "0.5.2-dev0""
+version = "0.5.3-dev0"
 author = 'Sathiyakugan Balakrishnan <bsathiyakugan@gmail.com>'
 homepage = 'https://github.com/sathiyakugan/MyHelloWorld'
 default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
